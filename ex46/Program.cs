a07e39226703cb1970e7ee24a8d0314e21b6ee2b
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-18 02:30:25 +0500

    1.0

--- ex46/Program.cs
+++ ex46/Program.cs
@@ -10,186 +10,212 @@ namespace ex46
     {
         static void Main(string[] args)
         {
-            Arena arena = new Arena();
-            List<Elemental> elementals = new List<Elemental>();
-            elementals.Add(new FireElemental("Элеменаталь огня", 100, 20, 0));
-            elementals.Add(new EarthElemental("Элементаль земли", 140, 20, 10));
-            elementals.Add(new WaterElement("Элементаль воды", 80, 20, 0));
-            elementals.Add(new WindElemental("Элементаль ветра", 100, 20, 0));
-            elementals.Add(new DarkElemenatal("Элементаль тьмы", 100, 30, 0));
-            elementals.Add(new LightElemental("Элементаль света", 100, 30, 0));
 
-            for (int i = 0; i < elementals.Count; i++)
-            {
-                Console.Write($"{i + 1}. ");
-                elementals[i].ShowStats();
-            }
-
-            Elemental firstElemental = arena.ChooseFirstElemental(elementals);
-            Elemental secondElemental = arena.ChooseSecondElemental(elementals);
-            
-            arena.Fight(firstElemental, secondElemental);
-
-            
         }
     }
+
     class Arena
     {
-        public Elemental ChooseFirstElemental(List<Elemental> elementals)
+        private List<Fighter> _fighters;
+        private int _maxHealth;
+        private int _maxDamage;
+
+        public Arena()
         {
-            int elementalNumber;
-            Console.Write("Выберете первого элементаля: ");
-
-            if (int.TryParse(Console.ReadLine(), out elementalNumber))
+            _maxHealth = 1000;
+            _maxDamage = 100;
+            _fighters = new List<Fighter>
             {
-                if (elementalNumber <= elementals.Count && elementalNumber > 0)
-                {
-                    Elemental firstElemental = elementals[elementalNumber -1];
-                    return firstElemental;
-                }
-                else
-                {
-                    Console.WriteLine("Такого элементаля нет...");
-                    return null;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Неккоректный ввод...");
-                return null;
-            }
-        }
-
-        public Elemental ChooseSecondElemental(List<Elemental> elementals)
-        {
-            int elementalNumber;
-            Console.Write("Выберете первого элементаля: ");
-
-            if (int.TryParse(Console.ReadLine(), out elementalNumber))
-            {
-                if (elementalNumber <= elementals.Count && elementalNumber > 0)
-                {
-                    Elemental secondElemental = elementals[elementalNumber -1];
-                    return secondElemental;
-                }
-                else
-                {
-                    Console.WriteLine("Такого элементаля нет...");
-                    return null;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Неккоректный ввод...");
-                return null;
-            }
-        }
-
-        public void Fight(Elemental firstElemental, Elemental secondElemental)
-        {
-            Console.Clear();
-
-            while (firstElemental.Health > 0 && secondElemental.Health > 0)
-            {
-                firstElemental.TakeDamage(secondElemental.Damage);
-                secondElemental.TakeDamage(firstElemental.Damage);
-                firstElemental.ShowCurrentHealth();
-                secondElemental.ShowCurrentHealth();
-            }
+                new Warlock("Чернокнижник", _maxHealth, _maxDamage, _maxDamage/5),
+                new Rogue("Разбойник", _maxHealth, _maxDamage, _maxDamage*2),
+                new Warrior("Воин", _maxHealth, _maxDamage, _maxDamage/2),
+                new Paladin("Паладин", _maxHealth, _maxDamage),
+                new Mage("Маг", _maxHealth, _maxDamage),
+                new Hunter("Охотник", _maxHealth, _maxDamage),
+                new Shaman("Шаман", _maxHealth, _maxDamage),
+                new Druid("Друид", _maxHealth, _maxDamage),
+                new Priest("Жрец", _maxHealth, _maxDamage)
+            };
         }
     }
 
-    class Elemental
+    class Fighter
     {
-        public string Name;
-        public int Health;
-        public int Damage;
-        public int Armor;
-
-        public Elemental(string name, int health, int damage, int armor)
+        public Fighter(string name, int health, int damage)
         {
             Name = name;
-            Health = health;
+            MaxHealth = health;
+            CurrentHealth = health;
             Damage = damage;
-            Armor = armor;
         }
 
-        public void TakeDamage(int damage)
-        {
-            Health -= damage - Armor;
-        }
+        public string Name { get; private set; }
+        public int CurrentHealth { get; set; }
+        public int MaxHealth { get; private set; }
+        public int Damage { get; set; }
 
         public void ShowStats()
         {
-            Console.WriteLine($"{Name}\n" +
-                $"Здоровье: {Health}\n" +
-                $"Наносимый урон: {Damage}\n" +
-                $"Броня: {Armor}\n");
+            Console.WriteLine($"{Name}\nЗдоровье: {CurrentHealth}\nУрон: {Damage}");
         }
 
         public void ShowCurrentHealth()
         {
-            Console.WriteLine($"{Name}\nHP: {Health}");
+            Console.WriteLine($"{Name}\nЗдоровье: {CurrentHealth}");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHealth -= damage;
         }
     }
 
-    class FireElemental : Elemental
+    class Warlock : Fighter
     {
-        public FireElemental(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
-
-        public void BurnArmor(int armor)
+        public Warlock(string name, int health, int damage, int lifesteal) : base(name, health, damage)
         {
-            armor -= 20;
+            Lifesteal = lifesteal;
+        }
+
+        public int Lifesteal { get; private set; }
+
+        public void StealLife()
+        {
+            CurrentHealth += Lifesteal;
+
+            if (CurrentHealth > MaxHealth)
+                CurrentHealth = MaxHealth;
         }
     }
 
-    class EarthElemental : Elemental
+    class Rogue : Fighter
     {
-        public EarthElemental(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
-
-        public void PutStoneShield()
+        public Rogue(string name, int health, int damage, int criticalDamage) : base(name, health, damage)
         {
-            Armor += 20;
+            CriticalDamage = criticalDamage;
+        }
+
+        public int CriticalDamage { get; private set; }
+
+        public void DealCriticalDamage()
+        {
+            Random random = new Random();
+            int chance = random.Next(5); //20%
+
+            if (chance == 0)
+                Damage = CriticalDamage;
         }
     }
 
-    class WaterElement : Elemental
+    class Warrior : Fighter
     {
-        public WaterElement(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
-
-        public void Heal()
+        public Warrior(string name, int health, int damage, int armor) : base(name, health, damage)
         {
-            Health += 20;
+            Armor = armor;
+        }
+
+        public int Armor { get; private set; }
+
+        public void BlockDamage(int damage)
+        {
+            damage -= Armor;
         }
     }
 
-    class WindElemental : Elemental
+    class Paladin : Fighter
     {
-        public WindElemental(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
+        public Paladin(string name, int health, int damage) : base(name, health, damage) { }
 
-        public void Dodge()
+        public void EvadeDamage(int damage)
         {
+            Random random = new Random();
+            int chance = random.Next(20); //5%
 
+            if (chance == 0)
+                damage = 0;
         }
     }
 
-    class DarkElemenatal : Elemental
+    class Mage : Fighter
     {
-        public DarkElemenatal(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
-
-        public void TakeLife()
+        public Mage(string name, int health, int damage) : base(name, health, damage)
         {
+            HitCount = 0;
+        }
 
+        public int HitCount { get; private set; }
+
+        public void BurnEnemy()
+        {
+            HitCount++;
+
+            for (int i = 0; i < HitCount; i++)
+                Damage += Damage / 10;
         }
     }
 
-    class LightElemental : Elemental
+    class Hunter : Fighter
     {
-        public LightElemental(string name, int health, int damage, int armor) : base(name, health, damage, armor) { }
+        public Hunter(string name, int health, int damage) : base(name, health, damage) { }
 
-        public void Consecrate()
+        public void HitToWounds(int health, int maxHealth)
         {
-            Damage += 20;
+            int extraDamage = health / 8;
+
+            if (health <= maxHealth / 2)
+                IncreaseDamage(extraDamage);
+            else if (health <= maxHealth / 3)
+                IncreaseDamage(extraDamage);
+            else if (health <= maxHealth / 4)
+                IncreaseDamage(extraDamage);
+        }
+
+        private void IncreaseDamage(int extraDamage)
+        {
+            Damage += extraDamage;
+        }
+    }
+
+    class Shaman : Fighter
+    {
+        public Shaman(string name, int health, int damage) : base(name, health, damage) { }
+
+        public void GetSpontaneousEffect()
+        {
+            Random random = new Random();
+            int chance = random.Next(4); //25%
+            int randomValue = random.Next(MaxHealth / 20, MaxHealth / 10);
+
+            if (chance == 0)
+                CurrentHealth += randomValue;
+            else if (chance == 1)
+                Damage += randomValue;
+            else if (chance == 2)
+                CurrentHealth -= randomValue;
+            else if (chance == 3)
+                Damage -= randomValue;
+        }
+    }
+
+    class Druid : Fighter
+    {
+        public Druid(string name, int health, int damage) : base(name, health, damage) { }
+
+        public void HealYourself()
+        {
+            if (CurrentHealth <= MaxHealth / 2)
+                CurrentHealth += CurrentHealth / 6;
+        }
+    }
+
+    class Priest : Fighter
+    {
+        public Priest(string name, int health, int damage) : base(name, health, damage) { }
+
+        public void RiseAgain()
+        {
+            if (CurrentHealth <= 0)
+                CurrentHealth += MaxHealth / 5;
         }
     }
 }
